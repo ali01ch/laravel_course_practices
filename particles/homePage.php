56f09034b990811bc789d2e954c93edac5e40054
Ali Chenini <ali.chenini.dev@gmail.com>
2024-01-03 13:54:01 +0330

    update the content of home page

--- particles/homePage.php
+++ particles/homePage.php
@@ -1,3 +1,4 @@
 <div class="container row mx-auto border border-secondary w-auto py-4">
-    <h1>صفحه نخست</h1>
+    <h2 class="border text-center text-primary my-3">سامانه متمرکز و جامع تحویل دهی تمرینات دوره لاراول</h2>
+    <h4 class="border text-center text-danger my-3">از این پس تمامی تمرینات در این بستر انجام خواهد شد</h4>
 </div>
